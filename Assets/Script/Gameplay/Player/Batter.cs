f65be0f73b6c4c49ce1314fdcd0bdcaff27c7c65
gestgest <87427692+gestgest@users.noreply.github.com>
2025-08-05 08:07:57 +0900

    batter debug

--- Assets/Script/Gameplay/Player/Batter.cs
+++ Assets/Script/Gameplay/Player/Batter.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,12 +6,63 @@ using UnityEngine;
 public class Batter : Player
 {
     //[SerializeField] private Baseball _ball;
+    private int base_index = 0;
+    [SerializeField] private Transform[] bases;
+
+    private bool isMove = false;
+    
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Debug.Log("잉잉");
+            IsMove = !IsMove;
+        }
+        InBase();
+    }
 
     public void DebugHitting()
     {
-        _ball.RemovePlayer();
+        //_ball.RemovePlayer();
 
-        _myBall.transform.position = transform.position + new Vector3(0.0f, 0.5f, 0.0f);
+        //_myBall.transform.position = transform.position + new Vector3(0.0f, 0.5f, 0.0f);
+        IsMove = true;
+    }
+
+    private void MoveBase()
+    {
+        nav.SetDestination(bases[base_index].position);
+        LookAtPlayer(bases[base_index].position);
+    }
+
+    private void InBase()
+    {
+        if (!isMove)
+        {
+            return;
+        }
+        if (base_index >= bases.Length)
+        {
+            return;
+        }
+        if (Vector3.Distance(bases[base_index].position, transform.position) <= 0.3f)
+        {
+            base_index++;
+            MoveBase();
+        }
+    }
+
+    public bool IsMove
+    {
+        get => isMove;
+        set
+        {
+            isMove = value;
+            if (isMove)
+            {
+                MoveBase();
+            }
+        }
     }
 
 }
