76be660bdbfa482a85d4d3cd6451c972d43cf7e8
gestgest <87427692+gestgest@users.noreply.github.com>
2025-09-02 07:06:22 +0900

    implement grab bat near mode (2/3)

--- Assets/Script/FarNearGrab.cs
+++ Assets/Script/FarNearGrab.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
+
+//IFarAttachProvider
 public class FarNearGrab : XRGrabInteractable
 {
     
@@ -14,18 +16,23 @@ public class FarNearGrab : XRGrabInteractable
     protected override void OnSelectEntered(SelectEnterEventArgs args)
     {
         base.OnSelectEntered(args);
-        
-        // 어느 손으로 잡았는지 확인
-        string interactorName = args.interactorObject.transform.name.ToLower();
-        
-        if (interactorName.Contains("left") && leftHandGrip != null)
-        {
-            transform.position = leftHandGrip.position;
-        }
-        else if (interactorName.Contains("right") && rightHandGrip != null)
-        {
-            transform.position = rightHandGrip.position;
-        }
+        //
+        // // 어느 손으로 잡았는지 확인
+        // //string interactorName = args.interactorObject.transform.name.ToLower();
+        // string mytag = args.interactorObject.transform.tag;
+        //
+        // Debug.Log("으아으아 : " + mytag );
+        //
+        // if (mytag.Contains("left") && leftHandGrip != null)
+        // {
+        //     transform.position = leftHandGrip.position;
+        //     Debug.Log("left : " + transform.position );
+        // }
+        // else if (mytag.Contains("right") && rightHandGrip != null)
+        // {
+        //     transform.position = rightHandGrip.position;
+        //     Debug.Log("right : " + transform.position );
+        // }
         
         //Debug.Log($"{interactorName} hold by controller. AttachTransform: {this.attachTransform.name}");
     }
