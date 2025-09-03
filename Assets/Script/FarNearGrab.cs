181449c691c4ef856ead79ffad71044606433d98
gestgest <87427692+gestgest@users.noreply.github.com>
2025-09-03 16:26:33 +0900

    implement move far bat

--- Assets/Script/FarNearGrab.cs
+++ Assets/Script/FarNearGrab.cs
@@ -15,13 +15,23 @@ public class FarNearGrab : XRGrabInteractable
     
     protected override void OnSelectEntered(SelectEnterEventArgs args)
     {
+        // Ray Interactor로 선택된 경우 => 일단 계속 건드려 보자
+        if (args.interactorObject is XRRayInteractor)
+        {
+            Debug.Log("포지셩" + args.interactorObject.transform.position);
+            // attachTransform을 컨트롤러 위치로 리셋
+            //attachTransform.position = args.interactorObject.transform.position;
+        }
         base.OnSelectEntered(args);
+
+        
+        
+        
         //
         // // 어느 손으로 잡았는지 확인
         // //string interactorName = args.interactorObject.transform.name.ToLower();
         // string mytag = args.interactorObject.transform.tag;
         //
-        // Debug.Log("으아으아 : " + mytag );
         //
         // if (mytag.Contains("left") && leftHandGrip != null)
         // {
